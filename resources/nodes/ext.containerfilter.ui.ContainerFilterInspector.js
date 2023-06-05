87a8017930caba4f318e8be657fe2bc1858a9051
miriam schlindwein <schlindwein@hallowelt.com>
2023-06-05 16:04:16 +0200

    Add droplet icon
    ERM32428
    
    [4.3.x]
    
    Change-Id: I91bed7fd4585a5b97c19a67893347d182c45a026
    (cherry picked from commit 13385bf55487d85b9f5db949aa6818210365983c)

--- resources/nodes/ext.containerfilter.ui.ContainerFilterInspector.js
+++ resources/nodes/ext.containerfilter.ui.ContainerFilterInspector.js
@@ -4,7 +4,9 @@ ext.containerfilter.ui = ext.containerfilter.ui || {};
 
 ext.containerfilter.ui.ContainerFilterInspector = function ( config ) {
 	// Parent constructor
-	ext.containerfilter.ui.ContainerFilterInspector.super.call( this, ve.extendObject( { padded: true }, config ) );
+	ext.containerfilter.ui.ContainerFilterInspector.super.call(
+		this, ve.extendObject( { padded: true }, config )
+	);
 };
 
 /* Inheritance */
@@ -17,7 +19,8 @@ ext.containerfilter.ui.ContainerFilterInspector.static.name = 'containerFilterIn
 
 ext.containerfilter.ui.ContainerFilterInspector.static.title = OO.ui.deferMsg( 'containerfilter-inspector-title' );
 
-ext.containerfilter.ui.ContainerFilterInspector.static.modelClasses = [ ext.containerfilter.dm.ContainerFilterNode ];
+ext.containerfilter.ui.ContainerFilterInspector.static.modelClasses =
+	[ ext.containerfilter.dm.ContainerFilterNode ];
 
 ext.containerfilter.ui.ContainerFilterInspector.static.dir = 'ltr';
 
@@ -73,13 +76,14 @@ ext.containerfilter.ui.ContainerFilterInspector.prototype.setLayouts = function 
  * @inheritdoc
  */
 ext.containerfilter.ui.ContainerFilterInspector.prototype.getSetupProcess = function ( data ) {
-	return ext.containerfilter.ui.ContainerFilterInspector.super.prototype.getSetupProcess.call( this, data )
-		.next( function () {
-			var attributes = this.selectedNode.getAttribute( 'mw' ).attrs;
+	return ext.containerfilter.ui.ContainerFilterInspector.super.prototype.getSetupProcess.call(
+		this, data
+	).next( function () {
+		var attributes = this.selectedNode.getAttribute( 'mw' ).attrs;
 
-			this.selectorInput.setValue( attributes.selector || '' );
-			this.actions.setAbilities( { done: true } );
-		}, this );
+		this.selectorInput.setValue( attributes.selector || '' );
+		this.actions.setAbilities( { done: true } );
+	}, this );
 };
 
 ext.containerfilter.ui.ContainerFilterInspector.prototype.wireEvents = function () {
@@ -87,7 +91,9 @@ ext.containerfilter.ui.ContainerFilterInspector.prototype.wireEvents = function 
 };
 
 ext.containerfilter.ui.ContainerFilterInspector.prototype.updateMwData = function ( mwData ) {
-	ext.containerfilter.ui.ContainerFilterInspector.super.prototype.updateMwData.call( this, mwData );
+	ext.containerfilter.ui.ContainerFilterInspector.super.prototype.updateMwData.call(
+		this, mwData
+	);
 
 	if ( this.selectorInput.getValue() !== '' ) {
 		mwData.attrs.selector = this.selectorInput.getValue();
@@ -99,9 +105,10 @@ ext.containerfilter.ui.ContainerFilterInspector.prototype.updateMwData = functio
 /**
  * @inheritdoc
  */
-ext.containerfilter.ui.ContainerFilterInspector.prototype.formatGeneratedContentsError = function ( $element ) {
-	return $element.text().trim();
-};
+ext.containerfilter.ui.ContainerFilterInspector.prototype.formatGeneratedContentsError =
+	function ( $element ) {
+		return $element.text().trim();
+	};
 
 /**
  * Append the error to the current tab panel.
--- resources/nodes/ext.containerfilter.ui.ContainerFilterInspectorTool.js
+++ resources/nodes/ext.containerfilter.ui.ContainerFilterInspectorTool.js
@@ -11,11 +11,12 @@ OO.inheritClass( ext.containerfilter.ui.ContainerFilterInspectorTool, ve.ui.Frag
 ext.containerfilter.ui.ContainerFilterInspectorTool.static.name = 'containerFilterTool';
 ext.containerfilter.ui.ContainerFilterInspectorTool.static.group = 'none';
 ext.containerfilter.ui.ContainerFilterInspectorTool.static.autoAddToCatchall = false;
-ext.containerfilter.ui.ContainerFilterInspectorTool.static.icon = 'filter';
+ext.containerfilter.ui.ContainerFilterInspectorTool.static.icon = 'funnel';
 ext.containerfilter.ui.ContainerFilterInspectorTool.static.title = OO.ui.deferMsg(
 	'containerfilter-inspector-title'
 );
-ext.containerfilter.ui.ContainerFilterInspectorTool.static.modelClasses = [ ext.containerfilter.dm.ContainerFilterNode ];
+ext.containerfilter.ui.ContainerFilterInspectorTool.static.modelClasses =
+	[ ext.containerfilter.dm.ContainerFilterNode ];
 ext.containerfilter.ui.ContainerFilterInspectorTool.static.commandName = 'containerFilterCommand';
 
 ve.ui.toolFactory.register( ext.containerfilter.ui.ContainerFilterInspectorTool );
