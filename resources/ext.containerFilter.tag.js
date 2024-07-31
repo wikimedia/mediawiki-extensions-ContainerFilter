6208e16ed4438988e7c44aba0aeeb5e0fab1da97
Mschlindwein <schlindwein@hallowelt.com>
2024-07-31 14:07:21 +0200

    Fix wcag - input label and announcement results
    ERM38237
    
    [4.5.x]
    
    Change-Id: Ia9eeb95f9351a4578a942e6c1f285d06cf419174
    (cherry picked from commit 803907b45ec5f5c6019010d13558ee44b8369f3c)

--- resources/ext.containerFilter.tag.js
+++ resources/ext.containerFilter.tag.js
@@ -2,8 +2,12 @@
 $( '.container-filter-search' ).each( function () {
 	var $searchField = $( this ),
 		$containerEl = $searchField.parent(),
-		searchField = OO.ui.infuse( $searchField );
+		searchField = OO.ui.infuse( $searchField ),
+		$resultCounter = $( '<div>' ).attr( 'aria-live', 'polite' ).addClass( 'visually-hidden' );
 	searchField.selector = $containerEl.data( 'selector' );
+
+	$( searchField.$input ).attr( 'aria-label', mw.message( 'containerfilter-input-aria-label' ).text() );
+	$containerEl.append( $resultCounter );
 	searchField.on( 'change', function ( value ) {
 		var normalValue = value.toLowerCase(),
 			$elementsToFilter = $( this.selector );
@@ -12,16 +16,21 @@ $( '.container-filter-search' ).each( function () {
 			$elementsToFilter.fadeIn();
 			return;
 		}
+		var results = 0;
 		$elementsToFilter.each( function () {
 			var $element = $( this ),
 				elementText = $element.text().toLowerCase();
 			if ( elementText.indexOf( normalValue ) !== -1 ) {
 				// eslint-disable-next-line no-jquery/no-fade
 				$element.fadeIn();
+				results += 1;
 			} else {
 				// eslint-disable-next-line no-jquery/no-fade
 				$element.fadeOut();
 			}
 		} );
+		$resultCounter.text(
+			mw.message( 'containerfilter-filter-results-label', results ).text()
+		);
 	}, [], searchField );
 } );
